5e79268f719091199316c821c20c158db6136724
Victor Dallagnol Bento <32740824+da3mons@users.noreply.github.com>
2018-06-10 23:42:19 -0300

    Update TemperatureSensor.c

--- TemperatureSensor.c
+++ TemperatureSensor.c
@@ -4,9 +4,10 @@
 void adc_configure(void);
 uint16_t adc_reader(uint8_t adc_num, uint8_t adc_chan);
 
-uint16_t result;	// Recebe a temperatura final
-uint16_t cu;
-struct adc_module adc_instance;
+uint16_t tf;	// Recebe a temperatura final
+uint16_t twotf; // teste
+
+struct adc_module adc_instance;	/* Estrutura para ADC*/
 
 /*
 *	CONFIGURAÇÃO
@@ -18,10 +19,10 @@ void adc_configure(void) {
 	adc_get_config_defaults(&config_adc);		// Configura ADC com valores iniciais
 		
 	config_adc.clock_source = GCLK_GENERATOR_0;	
-	//config_adc.resolution = 12;		// Taxa de conversão (+bits +lento)
+	//config_adc.resolution = 12;		// Taxa de conversão (+bits +lento)    <NECESSÁRIO ????? >
 	config_adc.positive_input = ADC_POSITIVE_INPUT_TEMP;	/* Configura a entrada positiva para
-																	* leitura da temperatura
-																	*/
+								 * leitura da temperatura
+								 */
 	config_adc.reference = ADC_REFERENCE_INT1V;
 	config_adc.negative_input = ADC_NEGATIVE_INPUT_GND;
 	/*
@@ -53,12 +54,13 @@ int main(void)
 	adc_start_conversion(&adc_instance);
 	
 
-	 // Wait for conversion to be done and read out result 
-	 while (adc_read(&adc_instance, &result) == STATUS_BUSY){}
-
-	 cu = 2*result;
-	 
-	 while (1) {
-		
-	 }
+	 /*
+	 *	Espera até que conversão seja feita e Resultado salvo em tempetaura
+	 */
+	 while (adc_read(&adc_instance, &tf) == STATUS_BUSY){}
+	
+	/* Testa Valor */ 
+	twotf = tf * 2;
+	
+	 while (1) {}
 }
